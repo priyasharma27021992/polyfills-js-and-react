16a58d1f60b0808a2c53219fc425a94f4812c116
Priya Sharma <priyasharma2702@gmail.com>
2025-11-27 23:45:45 +0100

    added some promise polyfil

--- promiseall-polyfil.ts
+++ promiseall-polyfil.ts
@@ -2,14 +2,13 @@ const promiseall = <T>(promises: Array<Promise<T>>) => {
     return new Promise((resolve, reject) => {
         const len = promises.length;
         if(len === 0){
-            return resolve([]);
+            resolve([]);
         }
         let remaining = len; //as substraction is better
         const result = new Array(len); // as fixed size arrays are better
 
         promises.forEach((promise, index) => {
             promise.then(data => {
-            console.log('result', result);
                 result[index] = data;
                 remaining--;
 
@@ -18,5 +17,20 @@ const promiseall = <T>(promises: Array<Promise<T>>) => {
                 }
             }).catch(error => reject(error));
         });
+    })
+}
+
+//race is promise which returns with whichever promise settles first, does not matter if it fulfilled or rejected
+const promiseRace = <T>(promises: Array<Promise<T>>) => {
+    return new Promise((resolve, reject) => {
+        const len = promises.length;
+        if(len === 0)
+            resolve([]);
+
+        promises.forEach((promise) => {
+            promise.then(data => {
+                resolve(data);
+            }).catch(error => reject(error));
+        })
     })
 }
